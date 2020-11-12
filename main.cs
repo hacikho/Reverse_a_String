3ebd6a32e9588d7d952761408f640d9a5f89ba32
Haci Karahasanoglu <hacikho01@gmail.com>
2020-11-12 04:21:33 +0000

    Add some validation

--- main.cs
+++ main.cs
@@ -2,10 +2,14 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine(Reverse("Haci Karahasanoglu"));
+    Console.WriteLine(Reverse("H"));
   }
 
   public static string Reverse(string input){
+    if(input == null || input.Length < 2 || input is string){
+      return " Something is wrong";
+    }
+    
     char[] charArray = input.ToCharArray();
     char[] tempArray = new char[charArray.Length];
 
