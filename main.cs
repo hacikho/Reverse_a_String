0fe79da51184589228faa16f7f4716e5d6f350bf
Haci Karahasanoglu <hacikho01@gmail.com>
2020-11-12 04:12:15 +0000

    working result

--- main.cs
+++ main.cs
@@ -2,6 +2,19 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine(Reverse("Haci Karahasanoglu"));
+  }
+
+  public static string Reverse(string input){
+    char[] charArray = input.ToCharArray();
+    char[] tempArray = new char[charArray.Length];
+
+    for(int i = 0; i < charArray.Length ; i++ ){
+      tempArray[i] = charArray[charArray.Length-1 - i]; 
+    }
+
+    string result = new string(tempArray);
+    return result;
+
   }
 }
